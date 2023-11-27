ddf2ce59830d3bf81b6c7ccb395cf5c5c712d6c2
fabriceboris <fabriceboris@gmail.com>
2023-11-27 15:29:11 +0100

    feat(Fronted):  add styling

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {CalculatorComponent} from "./calculator/calculator.component";
+import {MatTableModule} from "@angular/material/table";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CalculatorComponent],
+  imports: [CommonModule, RouterOutlet, CalculatorComponent, MatTableModule, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,15 @@
-import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {ApplicationConfig} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors} from "@angular/common/http";
 import {authenticationInterceptor} from "./auth/auth.interceptor";
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authenticationInterceptor]))
-  ]
+    provideHttpClient(withInterceptors([authenticationInterceptor])),
+    provideAnimations()
+]
 };
